3753ed78ef584226a0ffb726984b13e2689e07c6
heybys <byn0174@gmail.com>
2024-05-26 22:26:15 +0900

    update settings

--- deploy/admin-dockerize.sh
+++ deploy/admin-dockerize.sh
@@ -3,6 +3,11 @@
 # Set the name of the application
 APP_NAME="admin"
 
+# Declare color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 CONTAINER_NAME=$APP_NAME
 
 # Check if the container exists
@@ -17,9 +22,9 @@ if [ ! -z "$CONTAINER_EXISTS" ]; then
   docker rm $CONTAINER_NAME
 
   if [ $? -eq 0 ]; then
-    echo "Container $CONTAINER_NAME successfully deleted."
+    echo -e "${GREEN}✔${NC} Container $CONTAINER_NAME ${GREEN}Successfully deleted.${NC}"
   else
-    echo "Failed to delete container $CONTAINER_NAME."
+    echo -e "${RED}✘${NC} Container $CONTAINER_NAME ${RED}Failed to delete.${NC}"
   fi
 else
   echo "Container $CONTAINER_NAME not found. Nothing to delete."
@@ -38,13 +43,14 @@ if [ ! -z "$IMAGE_EXISTS" ]; then
   docker rmi $IMAGE_NAME
 
   if [ $? -eq 0 ]; then
-    echo "Image $IMAGE_NAME successfully deleted."
+    echo -e "${GREEN}✔${NC} Image $IMAGE_NAME ${GREEN}Successfully deleted.${NC}"
   else
-    echo "Failed to delete image $IMAGE_NAME."
+    echo -e "${RED}✘${NC} Image $IMAGE_NAME ${RED}Failed to delete.${NC}"
   fi
 else
   echo "Image $IMAGE_NAME not found. Nothing to delete."
 fi
+echo "------------------------------------------------Clear docker container/image completed------------------------------------------------"
 
 docker compose -f "apps/$APP_NAME/deploy/docker-compose.yml" build
 echo "------------------------------------------------Build docker image completed------------------------------------------------"
